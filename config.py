121aaf5010c2acd324c118ad52618da8bb183538
Justtery <916395242@qq.com>
2016-11-20 19:54:38 +0800

    加入关注人,被关注人,查看关注与被关注内容相关的内容,大概就是12章的内容.........

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flask Admin <916395242@qq.com>'
     FLASK_ADMIN = os.environ.get('FLASK-ADMIN')
     FLASK_POSTS_PER_PAGE = 10
+    FLASKY_FOLLOWERS_PER_PAGE = 10
+    FLASK_FOLLOWERS_PER_PAGE = 10
     @staticmethod
     def init_app(app):
         pass
